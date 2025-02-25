2782bb9c70d625abad7f33b9bfd8a9f30db3755e
Luiz <guga.santos0810@gmail.com>
2025-02-25 17:14:25 -0300

    optimization list

--- functions/functions.js
+++ functions/functions.js
@@ -18,7 +18,7 @@ function getType(type) {
   let span = document.createElement("span");
 
   span.style.backgroundColor = bg[type];
-  span.style.colro = cl[type];
+  span.style.color = cl[type];
   span.innerText = getFirstLatterUpperCase(type);
 
   return span;
@@ -36,4 +36,107 @@ function getFirstLatterUpperCase(name) {
   return `${firstLetter}${rest}`;
 }
 
-export { getType, getId, getFirstLatterUpperCase };
+function getName(name) {
+  if (name.includes("-f")) {
+    return getFirstLatterUpperCase(name.split("-f")[0]);
+  } else if (name.includes("-m")) {
+    return getFirstLatterUpperCase(name.split("-m")[0]);
+  } else {
+    return getFirstLatterUpperCase(name);
+  }
+}
+
+function searchByNameOrId() {
+  "https://pokeapi.co/api/v2/pokemon?limit=1304";
+}
+
+async function fetchPokemon(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+
+  const response2 = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${json.id}`
+  );
+  const json2 = await response2.json();
+  const newObject = { ...json, ...json2 };
+
+  return newObject;
+}
+
+async function fetchListPokemons() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1304`);
+  return await response.json();
+}
+
+function drawPokemon(json, showModal) {
+  let cards = document.getElementById("cards");
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let enDescription = json.flavor_text_entries.findLast(
+    (item) => item.language.name === "en"
+  );
+
+  let row2 = document.createElement("div");
+  row2.id = "row2";
+  row2.classList.add("row");
+
+  let row3 = document.createElement("div");
+  row3.id = "row3";
+  row3.classList.add("row");
+
+  const spans = json.types.map((type) => getType(type.type.name));
+
+  let span = document.createElement("span");
+  span.innerText = getId(json.id);
+
+  let row4 = document.createElement("div");
+  row4.id = "row4";
+  row4.classList.add("row");
+
+  let column1 = document.createElement("div");
+  column1.id = "column1";
+  column1.classList.add("column");
+
+  let h1 = document.createElement("h1");
+  h1.innerText = getName(json.name);
+
+  let p = document.createElement("p");
+  p.innerText = enDescription.flavor_text;
+
+  let button = document.createElement("button");
+  button.innerText = "Know more";
+  button.onclick = () => showModal(json.id);
+
+  let img = document.createElement("img");
+  img.src = json.sprites.other["official-artwork"].front_default;
+  img.alt = json.name;
+
+  spans.forEach((item) => row3.appendChild(item));
+
+  row2.appendChild(row3);
+  row2.appendChild(span);
+
+  column1.appendChild(h1);
+  column1.appendChild(p);
+  column1.appendChild(button);
+
+  row4.appendChild(column1);
+  row4.appendChild(img);
+
+  card.appendChild(row2);
+  card.appendChild(row4);
+
+  cards.appendChild(card);
+}
+
+export {
+  getType,
+  getId,
+  getFirstLatterUpperCase,
+  getName,
+  fetchPokemon,
+  fetchListPokemons,
+  drawPokemon,
+};
--- main.js
+++ main.js
@@ -1,10 +1,65 @@
-let offset = 0;
-let cards = document.getElementById("cards");
+import {
+  getType,
+  getId,
+  getFirstLatterUpperCase,
+  fetchPokemon,
+  fetchListPokemons,
+  drawPokemon,
+} from "./functions/functions.js";
+
+let begin = 0;
+let end = 6;
+let listOfAllPokemons;
+
+async function start() {
+  let { results } = await fetchListPokemons();
+
+  listOfAllPokemons = results;
+
+  for (let y = begin; y <= end; y++) {
+    const pokemon = await fetchPokemon(listOfAllPokemons[y].url);
+    drawPokemon(pokemon, showModal);
+  }
+}
+
+start();
+
 let isLoading = false;
+
 let modal = document.getElementById("modal");
 let close = document.getElementById("close");
 
-close.onclick = () => modal.classList.add("invisible");
+const img = modal.querySelector("img"); //img
+
+const column2 = document.getElementById("column2");
+const idText = column2.querySelector("span"); //id
+const row5 = document.getElementById("row5");
+
+const name = row5.querySelector("h1"); //name
+
+const playAudio = row5.querySelector("button");
+const audio = row5.querySelector("audio");
+const source = row5.querySelector("source");
+
+const row6 = document.getElementById("row6");
+
+const p = column2.querySelector("p"); //description
+
+const neight = document.querySelectorAll(".neight");
+
+const height = neight[0].querySelectorAll("span")[1]; //height
+const weight = neight[1].querySelectorAll("span")[1]; //weight
+
+const stats = document.querySelectorAll(".stats");
+
+const hp = stats[0].querySelectorAll("span"); //hp
+const atk = stats[1].querySelectorAll("span"); //atk
+const def = stats[2].querySelectorAll("span"); //def
+const spa = stats[3].querySelectorAll("span"); //spa
+const spd = stats[4].querySelectorAll("span"); //spd
+const spe = stats[5].querySelectorAll("span"); //spe
+
+close.onclick = closeModal;
 
 window.addEventListener("scroll", () => {
   if (isLoading) return;
@@ -16,67 +71,31 @@ window.addEventListener("scroll", () => {
 
   if (scroll + 25 >= total) {
     isLoading = true;
-    offset += 6;
+    begin += 6;
+    end += 6;
     start().finally(() => {
       isLoading = false;
     });
   }
 });
 
-import {
-  getType,
-  getId,
-  getFirstLatterUpperCase,
-} from "./functions/functions.js";
-
-async function fetchListPokemons() {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=6&offset=${offset}`
-  );
-  return await response.json();
-}
-
-async function fetchPokemon(url) {
-  const response = await fetch(url);
-  const json = await response.json();
-
-  const response2 = await fetch(
-    `https://pokeapi.co/api/v2/pokemon-species/${json.id}`
-  );
-  const json2 = await response2.json();
-  const newObject = { ...json, ...json2 };
-
-  return newObject;
-}
-
 async function showModal(id) {
   modal.classList.remove("invisible");
 
-  const column2 = document.getElementById("column2");
-  const idText = column2.querySelector("span");
-
   const poke = await fetchPokemon(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  console.log(poke);
 
-  const img = modal.querySelector("img");
   img.src = poke.sprites.other["official-artwork"].front_default;
   img.alt = poke.name;
 
   idText.innerText = getId(poke.id);
 
-  const row5 = document.getElementById("row5");
-  const name = row5.querySelector("h1");
   name.innerText = getFirstLatterUpperCase(poke.name);
 
-  const playAudio = row5.querySelector("button");
-  const audio = row5.querySelector("audio");
-  const source = row5.querySelector("source");
   source.src = poke.cries.latest;
   audio.load();
 
   playAudio.onclick = () => audio.play();
 
-  const row6 = document.getElementById("row6");
   Array.from(row6.childNodes).forEach((item) => row6.removeChild(item));
 
   for (const type of poke.types) {
@@ -88,104 +107,38 @@ async function showModal(id) {
     (item) => item.language.name === "en"
   );
 
-  const p = column2.querySelector("p");
   p.innerText = enDescription.flavor_text;
-
-  const neight = document.querySelectorAll(".neight");
-  const height = neight[0].querySelectorAll("span")[1];
-  const weight = neight[1].querySelectorAll("span")[1];
 
   height.innerText = `${(poke.height / 10).toFixed(1)}m`;
   weight.innerText = `${(poke.weight / 10).toFixed(1)}kg`;
 
-  const stats = document.querySelectorAll(".stats");
-
-  const hp = stats[0].querySelectorAll("span");
   hp[1].innerText = poke.stats[0].base_stat;
 
-  const atk = stats[1].querySelectorAll("span");
   atk[1].innerText = poke.stats[1].base_stat;
 
-  const def = stats[2].querySelectorAll("span");
   def[1].innerText = poke.stats[2].base_stat;
 
-  const spa = stats[3].querySelectorAll("span");
   spa[1].innerText = poke.stats[3].base_stat;
 
-  const spd = stats[4].querySelectorAll("span");
   spd[1].innerText = poke.stats[4].base_stat;
 
-  const spe = stats[5].querySelectorAll("span");
   spe[1].innerText = poke.stats[5].base_stat;
 }
 
-function drawPokemon(json) {
-  let card = document.createElement("div");
-  card.classList.add("card");
+function closeModal() {
+  modal.classList.add("invisible");
 
-  let enDescription = json.flavor_text_entries.findLast(
-    (item) => item.language.name === "en"
-  );
-
-  let row2 = document.createElement("div");
-  row2.id = "row2";
-  row2.classList.add("row");
-
-  let row3 = document.createElement("div");
-  row3.id = "row3";
-  row3.classList.add("row");
-
-  const spans = json.types.map((type) => getType(type.type.name));
-
-  let span = document.createElement("span");
-  span.innerText = getId(json.id);
-
-  let row4 = document.createElement("div");
-  row4.id = "row4";
-  row4.classList.add("row");
-
-  let column1 = document.createElement("div");
-  column1.id = "column1";
-  column1.classList.add("column");
-
-  let h1 = document.createElement("h1");
-  h1.innerText = getFirstLatterUpperCase(json.name);
-
-  let p = document.createElement("p");
-  p.innerText = enDescription.flavor_text;
-
-  let button = document.createElement("button");
-  button.innerText = "Know more";
-  button.onclick = () => showModal(json.id);
-
-  let img = document.createElement("img");
-  img.src = json.sprites.other["official-artwork"].front_default;
-  img.alt = json.name;
-
-  spans.forEach((item) => row3.appendChild(item));
-
-  row2.appendChild(row3);
-  row2.appendChild(span);
-
-  column1.appendChild(h1);
-  column1.appendChild(p);
-  column1.appendChild(button);
-
-  row4.appendChild(column1);
-  row4.appendChild(img);
-
-  card.appendChild(row2);
-  card.appendChild(row4);
-
-  cards.appendChild(card);
+  img.src =
+    "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
+  idText.innerText = "carregando...";
+  name.innerText = "carregando...";
+  p.innerText = "carregando...";
+  height.innerText = "...";
+  weight.innerText = "...";
+  hp.innerText = "...";
+  atk.innerText = "...";
+  def.innerText = "...";
+  spa.innerText = "...";
+  spd.innerText = "...";
+  spe.innerText = "...";
 }
-
-async function start() {
-  let list = await fetchListPokemons();
-  for (const item of list.results) {
-    const pokemon = await fetchPokemon(item.url);
-    drawPokemon(pokemon);
-  }
-}
-
-start();
